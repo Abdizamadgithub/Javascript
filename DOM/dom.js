0fc40f9729a6b3b777cd234069b7c429aeebdbd5
Abdizamadgithub <abdizamadali051@gmail.com>
2024-12-25 08:49:09 +0300

    Update

---
+++ DOM/dom.js
@@ -0,0 +1,36 @@
+// let elemnts = document.getElementById("title")
+// let test = document.getElementsByClassName("max")
+// let list = document.getElementsByTagName("li")
+// let query = document.querySelector(".max")
+
+// elemnts.style.color = "black"
+// elemnts.style.backgroundColor = "gray"
+// elemnts.style.padding = "20px";
+// elemnts.style.margin = "20px";
+// elemnts.style.border = "5px solid gray ";
+
+
+// test[0].style.color = "green"
+
+// list[1].style.color = "red"
+
+// query.style.color = "black"
+// query.style.backgroundColor = "gray"
+// query.style.padding = "20px";
+// query.style.margin = "20px";
+// query.style.border = "5px solid gray ";
+
+// let max = document.querySelector(".max")
+
+// max.addEventListener("click",() =>{
+//     document.hidden
+// })
+
+
+
+
+
+
+
+
+
